248b22a64025a2817885ed79cb17e8c8746dd0f8
Eyal Arviv <eyal8arviv@gmail.com>
2018-05-27 15:18:22 +0300

    final change

--- poi.js
+++ poi.js
@@ -2,30 +2,7 @@ var express = require('express');
 var router = express.Router();
 var DButilsAzure = require('./DButils');
 
-router.get('/getSingle/:name', function(req,res){
-    var name = req.params.name;
-//    DButilsAzure.execQuery("SELECT POI_name, NumOfViews, POI_description, POI_rank, Review1, Review2, PicturePath, Category FROM POI WHERE POI_name = '"+name+"'").then(function (recordSet) {   
-        DButilsAzure.execQuery("SELECT * FROM POI WHERE POI_name = '"+name+"'").then(function (recordSet) {   
-        res.json({
-            success: true,
-            POI_name: recordSet[0].POI_name,
-            NumOfViews: recordSet[0].NumOfViews,
-            POI_description: recordSet[0].POI_description,
-            POI_rank: recordSet[0].POI_rank,
-            Review1: recordSet[0].Review1,
-            Review2: recordSet[0].Review2,
-            PicturePath: recordSet[0].PicturePath,
-            Category: recordSet[0].Category
-        });
-   }).catch(function (err) {
-    res.send(err);
-        });
-        DButilsAzure.execQuery("UPDATE POI SET NumOfViews = NumOfViews + 1 WHERE POI_name = '"+name+"").then(function (recordSet) {   
-            res.json({ success: true, message: 'Point of interest is saved to favorites.' });
-        }).catch(function (err) {
-        res.send(err);
-               });
-})
+
 
 
 router.get('/all_poi', function(req,res){
@@ -37,6 +14,60 @@ router.get('/all_poi', function(req,res){
     
 })
 
+router.get('/:name', function(req,res){
+    var name = req.params.name;
+    var promises = [];
+    promises.push(DButilsAzure.execQuery("SELECT * FROM POI WHERE POI_name = '"+name+"'"));     
+    promises.push(DButilsAzure.execQuery("UPDATE POI SET NumOfViews = NumOfViews + 1 WHERE POI_name = '"+name+"'"));
+    Promise.all(promises).then(function(recordSet){
+        console.log(recordSet);
+        res.json({
+            success: true,
+            POI_name: recordSet[0][0].POI_name,
+            NumOfViews: recordSet[0][0].NumOfViews,
+            POI_description: recordSet[0][0].POI_description,
+            POI_rank: recordSet[0][0].POI_rank,
+            Review1: recordSet[0][0].Review1,
+            Review2: recordSet[0][0].Review2,
+            PicturePath: recordSet[0][0].PicturePath,
+            Category: recordSet[0][0].Category
+        })
+    }).catch(function (err) {
+        res.send(err);
+            });
+    /*
+    DButilsAzure.execQuery("SELECT * FROM POI WHERE POI_name = '"+name+"'")
+        .then(function (recordSet) {   
+
+
+
+        res.json({
+            success: true,
+            POI_name: recordSet[0].POI_name,
+            NumOfViews: recordSet[0].NumOfViews,
+            POI_description: recordSet[0].POI_description,
+            POI_rank: recordSet[0].POI_rank,
+            Review1: recordSet[0].Review1,
+            Review2: recordSet[0].Review2,
+            PicturePath: recordSet[0].PicturePath,
+            Category: recordSet[0].Category
+
+
+        });
+
+
+   }).catch(function (err) {
+    res.send(err);
+        });
+        
+
+        DButilsAzure.execQuery("UPDATE POI SET NumOfViews = NumOfViews + 1 WHERE POI_name = '"+name+"'").then(function (recordSet) {   
+            res.json({ success: true, message: 'Point of interest is saved to favorites.' });
+        }).catch(function (err) {
+        res.send(err);
+               });
+        */
+})
 
 router.get('/updatePOIs', function(req,res){
     DButilsAzure.execQuery("SELECT NumOfViews, POI_rank, Review1, Review2 FROM POI").then(function (recordSet) {   
