4b235d23bb58a619c55492e454579dcd7d7f758b
Eyal Arviv <eyal8arviv@gmail.com>
2018-06-11 00:01:30 +0300

    pois

--- routing/components/POI/poiCtrl.js
+++ routing/components/POI/poiCtrl.js
@@ -1,28 +1,45 @@
 angular.module('TripMe')
- .controller('poiCtrl', [function() {
- 
-
+ .controller('poiCtrl', ['$http', function($http) {
+  
     self = this;
 
-    self.cities = {
-        1 : {name:"Paris", state: "France", image: "https://media-cdn.tripadvisor.com/media/photo-s/0d/f5/7c/f2/eiffel-tower-priority.jpg"}
-        ,2 : {name:"Jerusalem", state: "Israel", image: "https://cdni.rt.com/files/2017.12/article/5a3fe04efc7e93cd698b4567.jpg"}
-        ,3 : {name:"London", state: "England", image: "http://www.ukguide.co.il/Photos/England/London/British-Royal-Tour.jpg"}
-    }
+    let serverUrl = 'http://localhost:3000/'
 
-    self.selectedCity= function (id){
+    
+   /* self.selectedCity= function (id){
 
         console.log (self.selected )
-    }
+    }*/
 
-    self.addToCart = function (id, city) {
+ /*   self.addToCart = function (id, city) {
 
         console.log(id)
         console.log(city)
         console.log(self.amount[id])
 
 
+    }*/
+
+    self.getAllPOIs = function(){
+        // register user
+        $http.get(serverUrl + "poi/all")
+        .then(function (response) {
+            let i = 0;
+            self.pois = {}
+            for (poi in response.data){
+                self.pois[i] = {name: response.data[i].POI_name, poi_img: response.data[i].PicturePath}
+                i++;
+            }
+        }, function (response) {
+            //Second function handles error
+            self.signUp.content = "Something went wrong";
+        });
     }
+    //self.getAllPOIs();
+
+    
+    
+    /*
     self.updatePOIs = function(){
         // register user
         $http.get(serverUrl + "poi/update", user)
@@ -47,18 +64,7 @@ angular.module('TripMe')
         });
     }
 
-    self.getAllPOIs = function(){
-        // register user
-        $http.get(serverUrl + "poi/all", user)
-        .then(function (response) {
-            //First function handles success
-            self.signUp.content = response.data;
-        }, function (response) {
-            //Second function handles error
-            self.signUp.content = "Something went wrong";
-        });
-    }
-
+    
     self.getOnePOI = function(){
         // register user
         $http.get(serverUrl + "poi/:name", user)
@@ -81,5 +87,5 @@ angular.module('TripMe')
             //Second function handles error
             self.signUp.content = "Something went wrong";
         });
-    }
+    }*/
 }]);
