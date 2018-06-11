c8b0e63c38978d7943b4223cf49130ba1b218480
hourish <hourish@post.bgu.ac.il>
2018-06-11 15:16:50 +0300

    cheeerrr

--- routing/components/About/aboutController.js
+++ routing/components/About/aboutController.js
@@ -1,7 +1,3 @@
 angular.module('TripMe')
 .controller('aboutController', ['$scope', function($scope) {
-    $scope.count = 0;
-    $scope.myFunc = function() {
-      $scope.count++;
-    };
   }]);
