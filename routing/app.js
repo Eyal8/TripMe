226ed5042b28b6933087cb63afebfd237428b23a
Eyal Arviv <eyal8arviv@gmail.com>
2018-06-12 20:41:24 +0300

    files hirarchy

--- routing/app.js
+++ routing/app.js
@@ -10,35 +10,35 @@ app.config(['$locationProvider', '$routeProvider', function($locationProvider, $
        // template: '<h1>This is the default route</h1>'
     })
         .when('/forgotPassword', {
-            templateUrl: 'components/ForgotPassword/forgotPassword.html',
+            templateUrl: 'routing/components/ForgotPassword/forgotPassword.html',
             controller : 'forgotPasswordController as frgtCtrl'
         })
         .when('/login', {
-            templateUrl: 'components/Login/login.html',
+            templateUrl: 'routing/components/Login/login.html',
             controller : 'loginController as lgnCtrl'
         })
         .when('/register', {
-            templateUrl: 'components/Register/register.html',
+            templateUrl: 'routing/components/Register/register.html',
             controller : 'registerController as rgCtrl'
         })
         .when('/about', {
-            templateUrl: 'components/About/about.html',
+            templateUrl: 'routing/components/About/about.html',
             controller : 'aboutController as abtCtrl'
         })
         .when('/guest', {
-            templateUrl: 'components/Guest/guest.html',
+            templateUrl: 'routing/components/Guest/guest.html',
             controller : 'guestCtrl as gstCtrl'
         })
         .when('/registered_users', {
-            templateUrl: 'components/RegisteredUsers/ru.html',
+            templateUrl: 'routing/components/RegisteredUsers/ru.html',
             controller: 'registeredUsersCtrl as ruCtrl'
         })
         .when('/singlePOI', {
-            templateUrl: 'components/SinglePOI/singlePOI.html',
+            templateUrl: 'routing/components/SinglePOI/singlePOI.html',
             controller: 'singlePOICtrl as sglCtrl'
         })
         .when('/service', {
-            templateUrl: 'components/Services/service.html',
+            templateUrl: 'shared/Services/Services/service.html',
             controller : 'serviceController as srvCtrl'
         })
         .otherwise({ redirectTo: '/' });
